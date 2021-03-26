7ee7d3bdd297d7a93a8fdcb03e15f40c3cbb4730
Manal <manalmaz01@gmail.com>
2021-03-27 01:47:30 +0300

    deleteduplicate

--- GoldenStay-WS/footer.php
+++
@@ -1,4 +0,0 @@
-<div id="footer">
-		<p><a href="index.php">Home</a> | <a href="contact.php">Contact Us</a></p>
-		<p>2019 &copy; Golden Stay </p>
-</div>
--- GoldenStay-WS/logoheader.php
+++
@@ -1,6 +0,0 @@
-<div id="logo">
-	<div id="logo_text">
-		<h1><a href="index.php">Golden<span class="logo_colour">Stay</span></a></h1>
-		<h2>Feeling Like Home</h2>
-	</div>
-</div>
